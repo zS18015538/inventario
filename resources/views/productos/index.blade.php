394cbfd2b93740066f43e46bd65f8e28a9d8a40f
Marco Antonio <57199708+zS18015538@users.noreply.github.com>
2024-11-02 12:53:24 -0600

    Cambio de vista Inventario por rol
    El menú de inventario productos en el rol de almacenista solo muestra los productos que están activos para poder dar salida.

--- resources/views/productos/index.blade.php
+++ resources/views/productos/index.blade.php
@@ -26,12 +26,38 @@
             </thead>
             <tbody>
                 @foreach($productos as $producto)
-                    <tr>
-                        <td>{{ $producto->idProducto }}</td>
-                        <td>{{ $producto->nombre }}</td>
-                        <td>{{ $producto->descripcion }}</td>
-                        <td>{{ $producto->cantidad }}</td>
-                        <td>{{ $producto->estatus ? 'Activo' : 'Inactivo' }}</td>
+                    @if($producto->estatus == 1 & Auth::user()->idRol == 2)
+                        <tr>
+                                <td>{{ $producto->idProducto }}</td>
+                                <td>{{ $producto->nombre }}</td>
+                                <td>{{ $producto->descripcion }}</td>
+                                <td>{{ $producto->cantidad }}</td>
+                                <td>{{ $producto->estatus ? 'Activo' : 'Inactivo' }}</td>
+                            
+                            @if(Auth::user()->idRol == 1)
+                                <td>
+                                    <a href="{{ route('productos.edit', $producto->idProducto) }}" class="edit-button">Editar</a>
+                                    <form action="{{ route('productos.destroy', $producto->idProducto) }}" method="POST" class="inline-form delete-form" data-title="¿Cambiar Estatus?" data-text="¿Quieres cambiar el estatus de este producto?">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="action-button">
+                                            {{ $producto->estatus ? 'Dar de Baja' : 'Reactivar' }}
+                                        </button>
+                                    </form>
+                                </td>
+                                <td>
+                                    <a href="{{ route('productos.agregarInventario', $producto->idProducto) }}" class="add-inventory-button">Agregar Inventario</a>
+                                </td>
+                            @endif
+                        </tr>
+                    @elseif (Auth::user()->idRol == 1)
+                        <tr>
+                            <td>{{ $producto->idProducto }}</td>
+                            <td>{{ $producto->nombre }}</td>
+                            <td>{{ $producto->descripcion }}</td>
+                            <td>{{ $producto->cantidad }}</td>
+                            <td>{{ $producto->estatus ? 'Activo' : 'Inactivo' }}</td>
+                        
                         @if(Auth::user()->idRol == 1)
                             <td>
                                 <a href="{{ route('productos.edit', $producto->idProducto) }}" class="edit-button">Editar</a>
@@ -48,6 +74,7 @@
                             </td>
                         @endif
                     </tr>
+                    @endif
                 @endforeach
             </tbody>
         </table>
